2787bb6fb2ce720a89ca7c650c0d2bb8a48b8bce
JesusZelayaG <jesus.zelayag@gmail.com>
2022-01-07 13:59:52 -0600

    Continuing the flutter course

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
-void main() {
+void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Heloo!'),);
+
+class _MyAppState extends State {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
